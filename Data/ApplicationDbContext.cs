78f5f5f932eb31cb158df75f90dc921ffd3d6fa5
Murilo Gotardo <murilogotardo08@gmail.com>
2023-01-13 20:06:34 -0300

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using vanadium.Models;
+
+namespace vanadium.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserViewModel> Livros { get; set; }
+    }
+}
---
+++ Models/BooksUserViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace vanadium.Models
+{
+	public class BooksUserViewModel
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public string Author { get; set; }
+
+
+
+	}
+}
