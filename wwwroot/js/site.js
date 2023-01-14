f4a723a8d543dfbbc152cbd904eba0a247ae48aa
Murilo Gotardo <murilogotardo08@gmail.com>
2023-01-14 16:29:40 -0300

    Melhorias gráficas na página de login

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 150 * i);
+    });
+}
+
+const titulo = document.querySelector('.escreve');
+
+typeWriter(titulo);
+
+const titulo2 = document.querySelector('.escreve2');
+
+typeWriter(titulo2);
